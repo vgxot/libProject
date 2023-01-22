e3dd0778b9f345ee00decc27a3045229225bf5fa
Vitaliy <107627295+vgxot@users.noreply.github.com>
2023-01-22 22:40:18 +0400

    delete reg-open-login
    попробую написать такое внутри vue приложения

--- frontend/js/reg-open-login.js
+++
@@ -1,5 +0,0 @@
-const btnLog = document.querySelector('#login-btn');
-const openBlock = document.querySelector('#login-block');
-btnLog.onclick = () => {
-    openBlock.classList.toggle('login-block-open');
-}
