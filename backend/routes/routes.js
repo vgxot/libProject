3e5fbba0b96ba5e3ae844dfd0408e6ce5ee964c9
Vitaliy <107627295+vgxot@users.noreply.github.com>
2023-01-24 23:54:56 +0400

    update routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -6,6 +6,10 @@ router.post('/user', userController.createUser)             // создает ю
 router.post('/user/update', userController.userBeAuthor)    // делает юзера автором
 router.delete('/user', userController.deleteUser)              // удаляет юзера
 router.post('/user/auth', userController.authUser)      // аутентификация юзера
-router.post('/user/logout', userController.logoutUser)  //
+router.post('/user/logout', userController.logoutUser)
+router.post('/user/getToken', userController.logoutUser)
+router.post('/user/token', userController.logoutUser)
+router.post('/user/upload', userController.logoutUser)
+router.get('/user/download', userController.logoutUser)//
 
 module.exports = router
