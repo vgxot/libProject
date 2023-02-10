b0d84d9a290516e148a4bbd7047a7ec863dce583
Vitaliy <107627295+vgxot@users.noreply.github.com>
2023-02-10 23:31:39 +0400

    update server.js
    удалил ненужный код, который отдает статику, теперь это дело Vue

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,10 @@
 const express = require('express')
-const path = require('path')
 const router = require(__dirname + '/routes/routes')
 const app = express()
 const cors = require('cors')
-const PORT = 3000;
+const PORT = 3001;
 
 app.use(express.json())
 app.use(cors())
 app.use('/api', router)
-app.use(express.static((path.resolve(__dirname + '/../frontend'))))
-
-app.get('/', function (req, res) {
-    res.sendFile(path.resolve(__dirname, '../frontend/main.html'))
-    res.status(200)
-})
-
 app.listen(PORT, () => console.log(`Сервер запущен. Порт ${PORT}`))
