ddc761594927e645cff0e54ee0fd056df057dc06
Vitaliy <107627295+vgxot@users.noreply.github.com>
2023-02-22 23:53:07 +0400

    choice-button update
    поменял названия стилей на более индивидуальные

--- frontend/src/UI/choice-button.vue
+++ frontend/src/UI/choice-button.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="button-block">
-    <my-button class="button">30</my-button>
-    <my-button class="button">50</my-button>
-    <my-button class="button">100</my-button>
+    <my-button class="button-choice">30</my-button>
+    <my-button class="button-choice">50</my-button>
+    <my-button class="button-choice">100</my-button>
   </div>
 </template>
 
@@ -17,21 +17,21 @@ export default {
 
 <style scoped>
 .button-block {
-  margin: 35px;
+  margin: 0 15px;
   background-color: #1E1E1E;
   width: max-content;
   border-radius: 17px;
   border: 4px solid #1E1E1E;
 }
-.button {
-  width: 100px;
+.button-choice {
+  width: 90px;
   height: 40px;
   border-radius: 13px;
 }
-.button:hover {
+.button-choice:hover {
   background-color: #1E1E1E;
 }
-.button:active {
+.button-choice:active {
   background-color: #303030;
 }
 </style>
