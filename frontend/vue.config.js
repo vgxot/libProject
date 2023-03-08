f706dfed489f246b56759e1e44bc18c397930ea7
Vitaliy <107627295+vgxot@users.noreply.github.com>
2023-03-08 14:48:27 +0400

    update protocol
    теперь сайт открывается по https://

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,3 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 8080,
+    https: true,
+  }
+}
