8f593e4a8178643c6f02d5f7450d744c8a38cd7f
Vitaliy <107627295+vgxot@users.noreply.github.com>
2023-03-14 23:27:09 +0400

    addition to statistics.vue

---
+++ frontend/src/UI/stat-item.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="stat-item">
+    <div class="stat-block">
+      <div class="stat-title a">{{ stat.name }}</div>
+      <div class="stat-value a">{{ stat.value }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "stat-item",
+  props: {
+    stat: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.stat-block {
+  width: max-content;
+  height: max-content;
+  padding: 10px 25px;
+  background-color: #1E1E1E;
+  border-radius: 25px;
+  margin: 10px;
+}
+.stat-title {
+  color: white;
+  font-size: 36px;
+}
+.stat-value {
+  font-size: 48px;
+  font-weight: bold;
+  background-image: linear-gradient(90deg, #d0b1fa,#ad68ff 40%,#9d4dff);
+  -webkit-background-clip: text;
+  color: rgba(255, 255, 255, 0);
+  width: max-content;
+}
+.a {
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
