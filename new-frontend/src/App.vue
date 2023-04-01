e9cde12ae90cd2980050812123ad1d92f5a8d04f
Vitaliy <107627295+vgxot@users.noreply.github.com>
2023-04-01 18:21:06 +0400

    create new vue app

---
+++ new-frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <router-view v-slot="{ Component }">
+    <transition name="question">
+      <component :is="Component" />
+    </transition>
+  </router-view>
+</template>
---
+++ new-frontend/src/components/UI/buttons/app-button.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="app-button">
+    <slot/>
+  </div>
+</template>
+
+<style scoped>
+.app-button {
+  position: relative;
+  border-radius: 20px;
+  background-color: #1e1e1e;
+  color: #ffffff;
+  text-align: center;
+  cursor: pointer;
+  transition: background-color 200ms ease-in-out,color 350ms ease-in-out;
+  text-decoration: none;
+  height: max-content;
+  width: max-content;
+  font-size: 24px;
+  padding: 10px 20px;
+  z-index: 1;
+  margin: 10px 0;
+}
+
+.app-button:hover {
+  background-color: #313131;
+  transition: background-color 200ms ease-in-out, color 200ms ease-in-out;
+}
+</style>
