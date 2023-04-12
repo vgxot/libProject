29bf810c8f1da53b03dd62c320a6a558526de2a7
Vitaliy <107627295+vgxot@users.noreply.github.com>
2023-04-12 23:31:35 +0400

    create pop-up-block

---
+++ frontend/src/components/UI/pop-up-s/popUpBlock.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="pop-up-block">
+    <slot/>
+  </div>
+</template>
+<script setup>
+</script>
+
+<style scoped>
+.pop-up-block {
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  width: 75%;
+  height: 400px;
+  margin: auto;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  text-align: center;
+  background-color: #1e1e1e;
+  z-index: 2;
+  border-radius: 25px;
+  align-items: center;
+  justify-content: space-between;
+  padding: 25px;
+}
+</style>
