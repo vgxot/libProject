336009ac57e21947bd448952cb1b36c706c70a28
Vitaliy <107627295+vgxot@users.noreply.github.com>
2023-04-13 22:59:46 +0400

    update routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -9,17 +9,17 @@ const other = require('../controls/other')
 router.post('/user', userControl.createUser)             // создает юзера
 router.post('/user/update', userControl.userBeAuthor)    // делает юзера автором
 router.delete('/user', userControl.deleteUser)              // удаляет юзера
-router.post('/user/auth', userControl.authUser)      // аутентификация юзера
+router.post('/user/login', userControl.loginUser)      // вход
 router.post('/user/logout', userControl.logoutUser)  // logout юзера
 router.get('/users/:id', userControl.getUser)               // получить данные о юзере
 router.get('/ratings/users/:id', userControl.getUserRatings)               // получить данные юзера о рейтинге, поставленном книгам
 router.get('/users', userControl.getUsers)               // получить всех юзеров
 router.post('/users/search', userControl.searchUsers)               // поиск юзера
 
+router.post('/token/refresh', userControl.refresh)
 router.post('/user/getToken', tokenControl.generateToken)   // generate token
-router.get('/user/token', tokenControl.saveToken)   // save token
 
-router.post('/books/upload', booksControl.booksUpload)               // загрузка книга на сервер
+router.post('/books/upload', booksControl.booksUpload)               // загрузка книги на сервер
 router.get('/books/download', booksControl.booksDownload)               // загрузка книги с сервера
 router.get('/books', booksControl.books)               // получить книги
 router.post('/books/search', booksControl.booksSearch)               // поиск книги
