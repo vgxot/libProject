2577247c6296fb07f2f403cb8fb9b7d96bdb2f9c
Vitaliy <107627295+vgxot@users.noreply.github.com>
2023-04-17 09:55:58 +0400

    create auth middleware
    эта штука будет проверять токен, 2 дня ушло на настройку авторизации JWT, вот теперь коммиты пушу

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,28 @@
+const tokenService = require("../services/tokenService")
+const ApiError = require("../exceptions/apiError");
+
+
+module.exports = function (req, res, next) {
+  try {
+    const authHeader = req.headers.authorization
+    console.log(authHeader)
+    if (!authHeader) {
+      console.log('пустой header')
+      return next(ApiError.UnauthorizedError());
+    }
+    const accessToken = authHeader.split(' ')[1];
+    if (!accessToken) {
+      console.log('пустой токен в header')
+      return next(ApiError.UnauthorizedError());
+    }
+    const userData = tokenService.validateAccessToken(accessToken);
+    if (!userData) {
+      console.log('не валидный токен')
+      return next(ApiError.UnauthorizedError());
+    }
+    req.user = userData;
+    next();
+  } catch (e) {
+    return next(ApiError.UnauthorizedError());
+  }
+};
