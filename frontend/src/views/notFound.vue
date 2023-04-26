48d1947ff991b1b861e70329cb8adbb482f7c930
Vitaliy <107627295+vgxot@users.noreply.github.com>
2023-04-26 22:33:17 +0400

    create notFound page

---
+++ frontend/src/views/notFound.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+import AppButton from "@/components/UI/buttons/app-button.vue";
+</script>
+
+<template>
+  <div class="not-found">
+    <p class="fs-72 white">404</p>
+    <app-button class="bkgd-dark">
+      <router-link to="/">На главную</router-link>
+    </app-button>
+  </div>
+</template>
+
+<style scoped>
+.not-found {
+  padding: 25%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
