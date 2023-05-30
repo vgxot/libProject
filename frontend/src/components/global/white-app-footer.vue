4a07e9a022e059461e5c35e9038b9bec3b6671a9
Vitaliy <107627295+vgxot@users.noreply.github.com>
2023-05-30 11:20:30 +0400

    added white footer

---
+++ frontend/src/components/global/white-app-footer.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AppFooter from "@/components/global/app-footer.vue";
+</script>
+
+<template>
+  <div style="background-color: white">
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<style scoped>
+</style>
